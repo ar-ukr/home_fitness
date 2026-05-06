2c00f296bd2e98e9f116332823fdfb8522644d43
ar-ukr <artem.ukrainchuk@gmail.com>
2026-05-06 19:48:53 +0300

    Update service worker cache version and assets
    Updated cache version and added new assets to cache.

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
-const CACHE_NAME = 'recomp-v1';
+const CACHE_NAME = 'recomp-v1.2';
 const ASSETS = [
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './sw.js'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Форсируем активацию новой версии SW сразу
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
     })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Удаляем старый кэш предыдущих версий
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Немедленно берем под контроль все вкладки
   );
 });
 
